aa65c47776d3cd3309daf06eed01fabda73c87d2
Natalia Miranda <natumiranda.28@gmail.com>
2026-03-23 19:14:00 -0300

    Improves input prompts for user clarity
    Adds colons to input prompts to enhance clarity and user experience during data entry.

--- ejercicio1_clase1.py
+++ ejercicio1_clase1.py
@@ -1,5 +1,5 @@
-nombre = input(" Ingrese su nombre ")
-apellido = input (" Ingrese su apellido ")
+nombre = input(" Ingrese su nombre: ")
+apellido = input (" Ingrese su apellido: ")
 
 print(f"Bienvenido/a {nombre} {apellido}")
 
