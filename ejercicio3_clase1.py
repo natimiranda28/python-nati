5b5e3d1ef7346fceb0a324ac6dc3c8edabf97dfd
Natalia Miranda <natumiranda.28@gmail.com>
2026-03-23 20:32:31 -0300

    ejercicio3 terminado

---
+++ ejercicio3_clase1.py
@@ -0,0 +1,18 @@
+
+"""Crea un programa que realice una suma, una resta,
+multiplicación y división,
+a partir de dos números ingresados por el usuario,
+y luego mostrar el resultado por pantalla
+"""
+
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+print(f"El resultado de la suma es: {numero1 + numero2}")
+print(f"El resultado de la resta es: {numero1 - numero2}")
+print(f"El resultado de la multiplicación es: {numero1 * numero2}")
+print(f"El resultado de la división es: {numero1 / numero2}") 
+""" esto dando por hecho que numero2 es distinto de cero. """
+
+
+
+
